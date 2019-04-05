bbadf438c1fa64e5b29028eb40d0afbcedca3248
Gette <37911525+AmBitSite@users.noreply.github.com>
2019-04-06 01:21:38 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,35 @@
+window.onload = ()=>{
+    (localStorage.length !==0)?hideLoginPage():($( ".nav" ).hide(), $(".content").hide());
+};
+
 let login = document.getElementById('input_login'),
     pass = document.getElementById('input_pass'),
     welcomePage = document.querySelector('.contain'),
     navBar = document.querySelector('.nav'),
     btnOut = document.querySelector('.nav__out');
 
-$( ".nav" ).hide();
+function hideLoginPage(){
+    $( ".contain" ).hide("slow");
+    $( ".nav" ).show("slow");
+    $(".content").show("slow");
+}
+
 welcomePage.addEventListener('keydown', (e)=>{
    if(e.keyCode == 13){
         if((login.value =='Pavel')&&(pass.value=='Rosolko')){
-            $( ".contain" ).hide( "slow", function() {
-                $( ".nav" ).show( "slow");
-            });
+            localStorage.setItem(`${login.value}`, `${pass.value}`);
+            hideLoginPage();
         };
     }
 });
 
 btnOut.addEventListener('click', ()=>{
-    $( ".nav" ).hide( "slow", function() {
-        $( ".contain" ).show( "slow");
-        });
+    $( ".nav" ).hide( "slow");
+    $( ".contain" ).show( "slow");
+    $(".content").hide("slow");
+    localStorage.removeItem(`${login.value}`);
 });
-        
+   
+
+
+
