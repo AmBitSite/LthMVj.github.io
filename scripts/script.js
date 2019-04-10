4568460935b7833131b543769493722a89b375e2
Gette <37911525+AmBitSite@users.noreply.github.com>
2019-04-10 16:36:28 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,78 @@
+window.onload = ()=>{
+    (localStorage.length !==0)?hideLoginPage():($( ".nav" ).hide(), $(".content").hide());
+};
+$(function(){
+    let step = '',
+        text = $('#loading').text()
+    function changeStep(){
+        $('#loading').text(text +step)
+        if(step == '...'){
+            step = ''
+        }
+        else {step +- '.'}
+    }
+    let interval = setInterval(changeStep, 500)
+    $(document).ready(function(){
+        setTimeout(function(){
+            clearInterval(interval)
+            $('#preloader').addClass('hide')
+        }, 2000);
+    });
+});
+
+let login = document.getElementById('input_login'),
+    welcomePage = document.querySelector('.contain'),
+    navBar = document.querySelector('.nav'),
+    btnOut = document.querySelector('.nav__out');
+    $(".nav__name").text(localStorage.key(localStorage.length-1));
+    let wrapper1 = document.querySelector('.content-menu');
+
+function hideLoginPage(){
+    $( ".contain" ).hide("slow");
+    $( ".nav" ).show("slow");
+    $(".content").show("slow");
+}
+
+welcomePage.addEventListener('keydown', (e)=>{
+   if(e.keyCode == 13){
+            localStorage.setItem(`${login.value}`, "1");
+            $(".nav__name").text(`${login.value}`);
+            hideLoginPage();
+    }
+});
+
+btnOut.addEventListener('click', ()=>{
+    $( ".nav" ).hide( "slow");
+    $( ".contain" ).show( "slow");
+    $(".content").hide("slow");
+    localStorage.removeItem($(".nav__name").text());
+});
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
