d8a035e30a19f0be651f997a5280ee8628acecde
Gette <37911525+AmBitSite@users.noreply.github.com>
2019-05-27 11:40:12 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -252,3 +252,47 @@ getGenre (compareNameFromZA); }
 else if (document.querySelectorAll('.sortOf')[0].value==2) {
 getGenre (compareNameShuffle); }
 }
+
+let context;
+let src;
+let analyser;
+
+function eqlzr() {
+    if (context == undefined) {
+        context = new AudioContext();
+        src = context.createMediaElementSource(currentTrack);
+        analyser = context.createAnalyser();
+    }
+    var canvas = document.getElementById("canvas");
+    canvas.width = "400";
+    canvas.height = "600";
+    var ctx = canvas.getContext("2d");
+    src.connect(analyser);
+    analyser.connect(context.destination);
+    analyser.fftSize = 256;
+    var bufferLength = analyser.frequencyBinCount;
+    var dataArray = new Uint8Array(bufferLength);
+    var WIDTH = canvas.width;
+    var HEIGHT = canvas.height;
+    var barWidth = (WIDTH / bufferLength) * 2.5;
+    var barHeight;
+    var x = 0;
+
+    function renderFrame() {
+        requestAnimationFrame(renderFrame);
+        x = 0;
+        analyser.getByteFrequencyData(dataArray);
+        ctx.fillStyle = "#352A3B";
+        ctx.fillRect(0, 0, WIDTH, HEIGHT);
+        for (var i = 0; i < bufferLength; i++) {
+            barHeight = dataArray[i];
+            var r = barHeight + (25 * (i / bufferLength));
+            var g = 250 * (i / bufferLength);
+            var b = 50;
+            ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+            ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+            x += barWidth + 1;
+        }
+    }
+    renderFrame();
+}
